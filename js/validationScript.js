1ba8da0ae90a1d58d2477e71125377c02ec59a76
carmenvanba <114095456+carmenvanba@users.noreply.github.com>
2022-11-17 11:58:40 +0100

    Inicio de script de validación

--- js/validationScript.js
+++ js/validationScript.js
@@ -1,41 +1,71 @@
 var alertRedInput = "#8C1010";
 var defaultInput = "green";
 
+//User Name Validation
 function userNameValidation(usernameInput) {
-    var username = document.getElementById("username");
+    var username = document.getElementById("userName");
     var issueArr = [];
-    if (/[-!@#$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(usernameInput)) {
-        issueArr.push("No special characters!");
-    }
-    if (issueArr.length > 0) {
-        username.setCustomValidity(issueArr);
+    if(document.getElementById("userName").value.length==0){
         username.style.borderColor = alertRedInput;
-    } else {
-        username.setCustomValidity("");
-        username.style.borderColor = defaultInput;
-    }
-}
+    }else{
+        if (/[-¡!@#$%^&*()_+|~=`{}\[\]:";'<>¿?,.\/]/.test(usernameInput)) {
+            issueArr.push("¡No usar caracteres especiales!");
+        }//Fin Si
+        if (issueArr.length > 0) {
+            username.setCustomValidity(issueArr);
+            username.style.borderColor = alertRedInput;
+        } else {
+            username.setCustomValidity("");
+            username.style.borderColor = defaultInput;
+        }//Fin Si
+    }//Fin Si
+}//Fin User Name Validation
 
+//User last name Validation
+function userLastNameValidation(userLastNameInput) {
+    var userLastName = document.getElementById("userLastName");
+    var issueArr = [];
+    if(document.getElementById("userLastName").value.length==0){
+        userLastName.style.borderColor = alertRedInput;
+    }else{
+        if (/[!¡@#$%^&*()_+|~=`{}\[\]:";<>¿?,.\/]/.test(userLastNameInput)) {
+            issueArr.push("¡No usar caracteres especiales!");
+        }//Fin Si
+        if (issueArr.length > 0) {
+            userLastName.setCustomValidity(issueArr);
+            userLastName.style.borderColor = alertRedInput;
+        } else {
+            userLastName.setCustomValidity("");
+            userLastName.style.borderColor = defaultInput;
+        }//Fin Si
+    }//Fin Si
+}//Fin User last name Validation
+
+//Password Validation
 function passwordValidation(passwordInput) {
     var password = document.getElementById("password");
     var issueArr = [];
-    if (!/^.{7,15}$/.test(passwordInput)) {
-        issueArr.push("Password must be between 7-15 characters.");
-    }
-    if (!/\d/.test(passwordInput)) {
-        issueArr.push("Must contain at least one number.");
-    }
-    if (!/[a-z]/.test(passwordInput)) {
-        issueArr.push("Must contain a lowercase letter.");
-    }
-    if (!/[A-Z]/.test(passwordInput)) {
-        issueArr.push("Must contain an uppercase letter.");
-    }
-    if (issueArr.length > 0) {
-        password.setCustomValidity(issueArr.join("\n"));
+    if(document.getElementById("password").value.length==0){
         password.style.borderColor = alertRedInput;
-    } else {
-        password.setCustomValidity("");
-        password.style.borderColor = defaultInput;
-    }
-}
+    }else{
+        if (!/^.{7,15}$/.test(passwordInput)) {
+            issueArr.push("La contraseña debe tener entre 7 y 15 caracteres.");
+        }//Fin Si
+        if (!/\d/.test(passwordInput)) {
+            issueArr.push("Debe tener al menos un número.");
+        }//Fin Si
+        if (!/[a-z]/.test(passwordInput)) {
+            issueArr.push("Debe tener al menos una letra minúscula.");
+        }//Fin Si
+        if (!/[A-Z]/.test(passwordInput)) {
+            issueArr.push("Debe tener al menos una letra mayúscula.");
+        }//Fin Si
+        if (issueArr.length > 0) {
+            password.setCustomValidity(issueArr.join("\n"));
+            password.style.borderColor = alertRedInput;
+        } else {
+            password.setCustomValidity("");
+            password.style.borderColor = defaultInput;
+        }//Fin Si
+    }//Fin Si
+}//Fin Password Validation
