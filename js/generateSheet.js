840bef327178b4c6ceb795a81dcd71a958563472
Carmenvanba <carvanrod94@gmail.com>
2022-11-21 19:48:38 +0100

    Ficha de Cuenta

--- js/generateSheet.js
+++ js/generateSheet.js
@@ -1,16 +1,26 @@
 //Variables
-var account = new Account();
+var plan;
+
+//Import Account from Storage
+var accountImport = JSON.parse(sessionStorage.getItem('accountSaved'));
 
 //Import Plan from Storage
-var accountImport = JSON.parse(sessionStorage.getItem('accountSaved'));
-account = accountImport;
-console.log(accountImport);
-console.log(account);
-console.log(account.getName());
+plan = sessionStorage.getItem('typePlan');
+//Change skin 
 
 
-console.log()
 
-
-//document.getElementById("prueba1").textContent = account.getName();
-//document.getElementById("prueba1").innerHTML = "El Nombre: "+account.getName();
+document.getElementById("name").innerHTML = accountImport["name"];
+document.getElementById("lastName").innerHTML = accountImport["lastName"];
+document.getElementById("email").innerHTML = accountImport["email"];
+document.getElementById("telNumber").innerHTML = accountImport["telNumber"];
+document.getElementById("country").innerHTML = accountImport["country"];
+document.getElementById("city").innerHTML = accountImport["city"];
+document.getElementById("address").innerHTML = accountImport["adress"];
+if(accountImport["creditCardNumber"] != null){
+    document.getElementById("creditCardNumber").innerHTML = accountImport["creditCardNumber"];
+    document.getElementById("paypalAccount").style.display= "none";
+}else{
+    document.getElementById("paypalAccount").innerHTML = accountImport["paypalAccount"];
+    document.getElementById("creditCardNumber").style.display= "none";
+}//FinSi
