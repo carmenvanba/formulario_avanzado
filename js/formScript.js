3558c78fe05ee39d322117716fbdf9f9989da359
carmenvanba <114095456+carmenvanba@users.noreply.github.com>
2022-11-22 22:34:54 +0100

    Inclusión barra progreso

--- js/formScript.js
+++ js/formScript.js
@@ -47,3 +47,27 @@ function paypalPayment(){
     document.getElementById("creditCardPayment").style.display = "none";
     document.getElementById("paypalPayment").style.display = "block";
 }
+
+
+/* Progress bar */
+var i = 1;
+$('.progress .circle').removeClass().addClass('circle');
+$('.progress .bar').removeClass().addClass('bar');
+
+    $('.progress .circle:nth-of-type(' + i + ')').addClass('active');
+
+    $('.progress .circle:nth-of-type(' + (i-1) + ')').removeClass('active').addClass('done');
+
+    $('.progress .circle:nth-of-type(' + (i-1) + ') .label').html('&#10003;');
+
+    $('.progress .bar:nth-of-type(' + (i-1) + ')').addClass('active');
+
+    $('.progress .bar:nth-of-type(' + (i-2) + ')').removeClass('active').addClass('done');
+
+    i++;
+
+    if (i==0) {
+        $('.progress .bar').removeClass().addClass('bar');
+        $('.progress div.circle').removeClass().addClass('circle');
+        i = 1;
+    }//Fin Si
